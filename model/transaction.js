2f659f9c761a16460a61f6f89a82c18523fc8e23
Hetvi <shahhetvi2404@gmail.com>
2024-07-02 11:13:46 +0530

    Added Background

--- model/transaction.js
+++ model/transaction.js
@@ -2,10 +2,22 @@ const mongoose = require("mongoose");
 
 // Define a schema and model for transactions
 const transactionSchema = new mongoose.Schema({
-    category: String,
-    amount: Number,
-    remark: String,
-    date: String,
-  });
+  category: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  remark: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+});
 
-  module.exports = mongoose.model("Transaction",transactionSchema);
+module.exports = mongoose.model("Transaction", transactionSchema);
