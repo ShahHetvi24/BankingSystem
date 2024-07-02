9a5f6e359ef23b5838fb22b10ad6ec42950bda57
Hetvi <shahhetvi2404@gmail.com>
2024-07-02 11:27:42 +0530

    Added Background

--- index.js
+++ index.js
@@ -4,27 +4,29 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 app.use(express.json());
-const route= require("./routes/route");
+const route = require("./routes/route");
 
 const port = process.env.PORT || 3000;
 
 const db_username = process.env.DB_USERNAME;
 const db_password = process.env.DB_PASSWORD;
 
-
 // Connect to MongoDB
 mongoose
-.connect(`mongodb+srv://${db_username}:${db_password}@bankingsystem.b1kd5k4.mongodb.net/`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Connected to MongoDB");
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-})
-.catch((err) => console.log(err));
+  .connect(
+    `mongodb+srv://${db_username}:${db_password}@bankingsystem.b1kd5k4.mongodb.net/`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
 
 app.use(express.static(__dirname));
 
@@ -32,4 +34,4 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.use("/transactions",route);
+app.use("/transactions", route);
